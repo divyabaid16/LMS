a2758e659b77c64a96f64b0492000cda88c5511d
divyabaid16 <divyabaid16@gmail.com>
2025-07-03 15:54:11 +0400

    Setting up basic frontend:

---
+++ projects/web/src/app/dashboard/instructor/page.tsx
@@ -0,0 +1,7 @@
+export default function InstructorDashboardPage() {
+  return (
+    <div className="p-6 text-2xl font-bold text-blue-600">
+      ✅ Hello from Instructor Dashboard
+    </div>
+  );
+}
---
+++ projects/web/src/app/dashboard/student/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function StudentDashboardPage() {
+  return (
+    <div>
+      <h1>StudentDashboardPage</h1>
+    </div>
+  );
+}
+
+export default StudentDashboardPage;
